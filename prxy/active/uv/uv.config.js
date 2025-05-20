945b288b8477854ce068fa4c4f67d1d7102db416
ejgavin <ejgavin11@gmail.com>
2025-05-20 19:28:22 -0400

    Initial commit

--- prxy/active/uv/uv.config.js
+++ prxy/active/uv/uv.config.js
@@ -16,12 +16,12 @@
 //  ⠀⠀⠀⠀⠀⠀⠐⢫⠏⢸⠁⠀⠈⢳⠘⢧⣙⢦⠀⠀⠀
 
 self.__uv$config = {
-  prefix: "/active/uv/service/",
+  prefix: "/W/active/uv/service/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/active/uv/uv.handler.js",
-  client: "/active/uv/uv.client.js",
-  bundle: "/active/uv/uv.bundle.js",
-  config: "/active/uv/uv.config.js",
-  sw: "/active/uv/uv.sw.js",
+  handler: "/W/active/uv/uv.handler.js",
+  client: "/W/active/uv/uv.client.js",
+  bundle: "/W/active/uv/uv.bundle.js",
+  config: "/W/active/uv/uv.config.js",
+  sw: "/W/active/uv/uv.sw.js",
 };
